2f5de1ff0a0f1676303bb9dc38c88200a24219d8
kbbushman <k.bushman@yahoo.com>
2021-03-12 16:14:20 -0800

    Adds initial apollo server config

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const { ApolloServer } = require('apollo-server');
+const Query = require('./resolvers/Query');
+
+const resolvers = {
+  Query,
+};
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8',
+  ),
+  resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+    };
+  },
+});
+
+server
+  .listen()
+  .then(({ url }) => console.log(`Server is running on ${url}`));
