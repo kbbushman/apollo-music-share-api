ecd1bba9f36e1c094a4e1c06886b584aaf783c03
kbbushman <k.bushman@yahoo.com>
2021-03-12 19:39:24 -0800

    Updates addSong data object with songInput, adds pubsub publish

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,13 +1,17 @@
 async function addSong(parent, args, context, info) {
   const newSong = await context.prisma.song.create({
     data: {
-      title: args.title,
-      artist: args.artist,
-      thumbnail: args.thumbnail,
-      url: args.url,
-      duration: args.duration,
+      title: args.songInput.title,
+      artist: args.songInput.artist,
+      thumbnail: args.songInput.thumbnail,
+      url: args.songInput.url,
+      duration: args.songInput.duration,
     }
   });
+
+  const songs = await context.prisma.song.findMany();
+
+  context.pubsub.publish('NEW_SONG', songs);
 
   return newSong;
 }
