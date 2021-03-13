031a23dedbc40b76173696dc37133e30159be5ee
kbbushman <k.bushman@yahoo.com>
2021-03-12 16:15:18 -0800

    Adds getSongs resolver with temp data

---
+++ src/resolvers/Query.js
@@ -0,0 +1,30 @@
+const TEMP_SONGS = [
+  {
+    id: 1,
+    title: 'Song One',
+    artist: 'Artist One',
+    thumbnail: 'http://example.com',
+    url: 'http://example.com',
+    duration: 255,
+    created_at: 'yesterday',
+    updated_at: 'today'
+  },
+  {
+    id: 2,
+    title: 'Song Two',
+    artist: 'Artist Two',
+    thumbnail: 'http://example.com',
+    url: 'http://example.com',
+    duration: 315,
+    created_at: 'yesterday',
+    updated_at: 'today'
+  }
+];
+
+function getSongs() {
+  return TEMP_SONGS;
+}
+
+module.exports = {
+  getSongs,
+};
